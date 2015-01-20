e44750b4d3f14e128fcc891b77d73895fd98fd0c
Keloran <keloran@nordicarts.net>
2015-01-20 20:25:52 +0000

    Updated

--- Experiments/Main.cpp
+++ Experiments/Main.cpp
@@ -56,7 +56,6 @@ namespace Game {
             printIt(oException.getMessage());
         } catch ( ... ) {
             handleException(std::current_exception());
-
         }
 
         return EXIT_SUCCESS;
