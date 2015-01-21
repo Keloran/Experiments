25f2d7486aababc20ad3a536bb02dffd8a9b7196
Keloran <keloran@develbox.info>
2015-01-21 21:19:10 +0000

    added suzanne

--- Experiments/Main.cpp
+++ Experiments/Main.cpp
@@ -4,6 +4,9 @@
 #include <NordicEngine/Logger/Logger.hpp>
 #include <NordicEngine/Settings/Settings.hpp>
 #include <NordicEngine/Renderer/Renderer.hpp>
+#include <NordicEngine/Files/Format/Obj/Obj.hpp>
+
+#include <NordicEngine/ThirdParty/glm/glm/glm.hpp>
 
 namespace Game {
     void handleException(std::exception_ptr oException) {
@@ -18,8 +21,8 @@ namespace Game {
 
     int Main() {
         try {    
+            // Set the Game Name
             std::string cGameName = "Experiments";
-
 
             // Logger Object
             NordicArts::NordicEngine::Logger  oLogger(cGameName);
@@ -28,6 +31,14 @@ namespace Game {
             // Settings Object
             NordicArts::NordicEngine::Settings  oSettings(pLogger, cGameName);
             NordicArts::NordicEngine::Settings *pSettings = &oSettings;
+
+            // Model Tester
+            std::vector<glm::vec3> vVertex;
+            std::vector<glm::vec3> vUV;
+            std::vector<glm::vec3> vNormal;
+    
+            NordicArts::NordicEngine::Files::Obj oObj("GameFiles/Models/suzanne.obj");
+            oObj.loadModel(vVertex, vUV, vNormal);
 
             // Setup settings
             pSettings->setup();
@@ -51,9 +62,9 @@ namespace Game {
                 pWindow->display();
             }
         } catch (std::exception &oException) {
-            throw NordicArts::NordicEngine::Exceptions(oException.what());
-        } catch (NordicArts::NordicEngine::Exceptions &oException) {
-            printIt(oException.getMessage());
+            throw NordicArts::NordicEngine::Exception(oException.what());
+        } catch (NordicArts::NordicEngine::Exception &oException) {
+            printIt(oException.what());
         } catch ( ... ) {
             handleException(std::current_exception());
         }
